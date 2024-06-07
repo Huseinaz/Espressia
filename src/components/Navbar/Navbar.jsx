810c9e9ebad3f5b6b3b2ee5f9ce15143cb360fdb
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-07 23:51:23 +0300

    Creating navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./Navbar.css";
+import { MenuItems } from "./MenuItems";
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="NavbarItems">
+            <h1 className="navbar-logo">Logo</h1>
+
+            <ul className="nav-menu">
+                {MenuItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a href="/">
+                                <i class={item.icon} /> {item.title}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
