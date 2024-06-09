f68744130d9ef86c25d7e0cb5202067d195a153e
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-09 15:13:53 +0300

    Adding clicked state

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
 import "./Navbar.css";
 import { MenuItems } from "./MenuItems";
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleSetClicked = () => {
+        setClicked(!clicked);
+    }
+
     return (
         <nav className="NavbarItems">
             <h1 className="navbar-logo">Logo</h1>
 
-            <ul className="nav-menu">
+            <div className="menu-icons" onClick={handleSetClicked}>
+                <i className={clicked ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+
+            <ul className={clicked ? "nav-menu active" : "nav-menu"}>
                 {MenuItems.map((item, index) => {
                     return (
                         <li key={index}>
                             <a className={item.cName} href="/">
-                                <i class={item.icon} /> {item.title}
+                                <i className={item.icon} /> {item.title}
                             </a>
                         </li>
                     );
