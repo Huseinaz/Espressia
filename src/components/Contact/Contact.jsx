d78aa9c83f808e21eb061aae825638f80df84ee3
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-10 17:08:27 +0300

    Correct typo

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <h1>Tis is Contact</h1>
+            <h1>This is Contact</h1>
         </div>
     )
 }
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Service = () => {
     return (
         <div>
-            <h1>Tis is Service</h1>
+            <h1>This is Services</h1>
         </div>
     )
 }
