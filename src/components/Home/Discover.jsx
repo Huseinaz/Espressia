387dc9eb85ba53c2f44cae18221139aaa455b8f0
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-21 16:35:30 +0300

    Create Discover.jsx

---
+++ src/components/Home/Discover.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Discover = () => {
+    return (
+        <div className='discover'>
+            <h1>Discover our premium coffee beans.</h1>
+            <p>At Espressia, we are passionate about providing you with the finest coffee experience.</p>
+        </div>
+    )
+}
+
+export default Discover
