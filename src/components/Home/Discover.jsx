424caf2cde794ae033c057a1e20ea0476bc7071c
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-22 22:48:52 +0300

    Update Discover.jsx

--- src/components/Home/Discover.jsx
+++ src/components/Home/Discover.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './Discover.css'
-
-import Coffee1 from '../../assets/coffee1.jpg'
-import Coffee2 from '../../assets/coffee2.jpg'
+import DiscoverData from './DiscoverData'
 
 const Discover = () => {
     return (
@@ -10,20 +8,7 @@ const Discover = () => {
             <h1>Discover our premium coffee beans.</h1>
             <p>At Espressia, we are passionate about providing you with the finest coffee experience.</p>
 
-            <div className='first-dis'>
-                <div className='dis-text'>
-                    <h2>Featured Coffees</h2>
-                    <p>
-                        <span>Single-Origin Delight:</span> Savor the unique flavors from coffee-growing regions around the world.<br />
-                        <span>Signature Blends:</span> Our expert roasters create perfect blends for a balanced and complex taste.<br />
-                        <span>Seasonal Specials:</span> Enjoy limited-time offerings crafted to capture the essence of each season.<br />
-                    </p>
-                </div>
-                <div className='image'>
-                    <img src={Coffee1} alt='img' />
-                    <img src={Coffee2} alt='img' />
-                </div>
-            </div>
+            <DiscoverData />
         </div>
     )
 }
