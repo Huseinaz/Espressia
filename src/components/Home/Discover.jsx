e9696f04a60b9b2c012c63e6392c2da642872443
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-22 23:06:21 +0300

    Update Discover.jsx

--- src/components/Home/Discover.jsx
+++ src/components/Home/Discover.jsx
@@ -8,7 +8,12 @@ const Discover = () => {
             <h1>Discover our premium coffee beans.</h1>
             <p>At Espressia, we are passionate about providing you with the finest coffee experience.</p>
 
-            <DiscoverData />
+            <DiscoverData 
+                heading = 'Featured Coffees'
+                text ='<span>Single-Origin Delight:</span> Savor the unique flavors from coffee-growing regions around the world.<br />
+                    <span>Signature Blends:</span> Our expert roasters create perfect blends for a balanced and complex taste.<br />
+                    <span>Seasonal Specials:</span> Enjoy limited-time offerings crafted to capture the essence of each season.<br />'
+            />
         </div>
     )
 }
