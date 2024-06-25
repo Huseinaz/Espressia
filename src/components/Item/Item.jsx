389fb70fa75dd580053c6a2caf7587cd38c840a4
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-25 22:40:33 +0300

    Create Item.jsx

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Item.css'
+
+const Item = () => {
+  return (
+    <div className='item'>
+        <h1>Our Best Sellers</h1>
+        <p>Customer favorites, crafted for the perfect coffee experience.</p>
+    </div>
+  )
+}
+
+export default Item;
