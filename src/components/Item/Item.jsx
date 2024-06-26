ea3184f92dbae6ae457cf3d401c8c990bb006749
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-26 15:26:56 +0300

    Update Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Item.css'
+import ItemData from './ItemData';
+
+import Item1 from '../../assets/CaramelMacchiato.jpg'
 
 const Item = () => {
   return (
     <div className='item'>
         <h1>Our Best Sellers</h1>
         <p>Customer favorites, crafted for the perfect coffee experience.</p>
+
+        <div className='item-card'>
+            <ItemData
+                image={Item1}
+                heading='Caramel Macchiato'
+                text='Enjoy the rich and smooth flavor of our Caramel Macchiato. This delightful drink combines our signature espresso with steamed milk and a sweet caramel drizzle, offering a perfect balance of sweetness and coffee richness.'
+            />
+        </div>
     </div>
   )
 }
