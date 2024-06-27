8de527fef313403e5281b0080455f9647fb92605
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-27 22:19:14 +0300

    Update Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -9,6 +9,20 @@ const Footer = () => {
                 <h1>Espressia</h1>
                 <p>The finest coffee experience awaits you</p>
             </div>
+            <div>
+                <a href='/'>
+                    <i className="fa-brands fa-facebook-square" />
+                </a>
+                <a href='/'>
+                    <i className="fa-brands fa-instagram-square" />
+                </a>
+                <a href='/'>
+                    <i className="fa-brands fa-linkedin" />
+                </a>
+                <a href='/'>
+                    <i className="fa-brands fa-whatsapp-square" />
+                </a>
+            </div>
         </div>
 
         <div className='bottom'>
