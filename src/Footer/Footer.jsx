1bd3114263105fafa2efee33ef5240b633d82361
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-27 22:19:07 +0300

    Create Footer.jsx

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='top'>
+            <div>
+                <h1>Espressia</h1>
+                <p>The finest coffee experience awaits you</p>
+            </div>
+        </div>
+
+        <div className='bottom'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
