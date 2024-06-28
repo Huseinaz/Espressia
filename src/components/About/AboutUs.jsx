866e768ee45567f37bbbb88d01852cc33b5b0217
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-06-28 22:33:22 +0300

    Create AboutUs.jsx

---
+++ src/components/About/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './About.css'
+
+const AboutUs = () => {
+    return (
+        <div className='about-container'>
+            <h1>About Espressia</h1>
+            <p>At Espressia, we believe that every cup of coffee tells a story. Our journey began with a simple love for coffee and a mission to bring the best beans from around the world to your cup.</p>
+
+            <h1>Our Mission</h1>
+            <p>Our mission is to elevate your coffee experience by providing high-quality, ethically sourced coffee. We are committed to sustainable practices and supporting the communities where our coffee is grown.</p>
+        </div>
+    )
+}
+
+export default AboutUs;
